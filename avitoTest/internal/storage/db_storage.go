34f1c24f37504aff9efb7fc79efa4fc45fc1a3f5
Mikhail Kislov <kislov.mik@yandex.ru>
2024-09-08 21:57:46 +0300

    initial commit

--- avitoTest/internal/storage/db_storage.go
+++ avitoTest/internal/storage/db_storage.go
@@ -35,26 +35,26 @@ func (p *DB) AddNewTender(ctx context.Context, tender model.Tender) (model.Tende
 	if err != nil {
 		return model.Tender{}, err
 	}
-
-	_, err = p.dbPool.Exec(ctx, `INSERT INTO tenders (id, name, organization_id, description, status, servicetype, version, createdat)
-				VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5, $6, $7)`,
-		tender.Name, tender.OrganizationID, tender.Description, tender.Status, tender.ServiceType, 1, time.Now())
-	return model.Tender{}, err
+	err = p.dbPool.QueryRow(ctx, `INSERT INTO tenders (id, name, organization_id, description, status, servicetype, version, createdat)
+			VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5, $6, $7)
+			RETURNING id, version, createdat`,
+		tender.Name, tender.OrganizationID, tender.Description, tender.Status, tender.ServiceType, 1, time.Now()).
+		Scan(&tender.ID, &tender.Version, &tender.CreatedAt)
+	return tender, err
 }
 
-func (p *DB) UpdateTenderStatus(ctx context.Context, tenderID, userId, status string) error {
+func (p *DB) UpdateTenderStatus(ctx context.Context, tenderID, userId, status string) (model.Tender, error) {
 	var validUserId string
 	err := p.dbPool.QueryRow(ctx, `SELECT user_id FROM organization_responsible o
             JOIN tenders t ON t.organization_id = o.organization_id
             WHERE t.id = $1 AND o.user_id = $2`, tenderID, userId).Scan(&validUserId)
 
 	if err != nil {
-		return err
+		return model.Tender{}, err
 	}
-	_, err = p.dbPool.Exec(context.Background(), `
-            UPDATE tenders
-            SET status = $1
-            WHERE id = $2
-        `, status, tenderID)
-	return err
+	var t model.Tender
+	err = p.dbPool.QueryRow(context.Background(), `UPDATE tenders SET status = $1 WHERE id = $2 
+            RETURNING id, name, organization_id, description, status, servicetype, version, createdat`, status, tenderID).
+		Scan(&t.ID, &t.Name, &t.OrganizationID, &t.Description, &t.Status, &t.ServiceType, &t.Version, &t.CreatedAt)
+	return t, err
 }
