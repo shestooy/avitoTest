d1df9f39490a2aba5e7220292cfd07a399f9b34b
shestooy <kmd017@campus.mephi.ru>
2024-09-10 14:44:00 +0300

    add struct bids

--- avitoTest/internal/server/model/model.go
+++ avitoTest/internal/server/model/model.go
@@ -11,3 +11,17 @@ type Tender struct {
 	OrganizationID string `json:"organizationId"`
 	UsernameID     string `json:"creatorUserName"`
 }
+
+type Bids struct {
+	ID              string `json:"id"`
+	Name            string `json:"name"`
+	Description     string `json:"description"`
+	Status          string `json:"status"`
+	TenderId        string `json:"tenderId"`
+	OrganizationId  string `json:"organizationId"`
+	CreatorUsername string `json:"creatorUsername"`
+	AuthorID        string `json:"authorID"`
+	AuthorType      string `json:"authorType"`
+	CreatedAt       string `json:"createdAt"`
+	Version         uint   `json:"version"`
+}
